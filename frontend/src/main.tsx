e8d1582add80042565f6976940b1d3604e20b02e
masoomehmokhtari78 <masoomehmokhtari693@gmail.com>
2026-06-14 00:10:12 +0330

    fix theme toggle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,16 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        {/* Only next-themes is required here to manage your global .dark class */}
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+          <App />
+        </NextThemesProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
